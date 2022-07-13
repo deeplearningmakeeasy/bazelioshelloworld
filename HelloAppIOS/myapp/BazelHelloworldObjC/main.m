718d2bd28322fa8c8b781f35b9e7650e9bda0a0c
deeplearningmakeeasy <deeplearningmakeeasy@gmail.com>
2022-07-13 23:30:07 +0800

    Main commit

---
+++ HelloAppIOS/myapp/BazelHelloworldObjC/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BazelHelloworldObjC
+//
+//  Created by ivan on 13/7/2022.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    NSString * appDelegateClassName;
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+        appDelegateClassName = NSStringFromClass([AppDelegate class]);
+    }
+    return UIApplicationMain(argc, argv, nil, appDelegateClassName);
+}
